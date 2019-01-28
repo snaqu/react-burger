49df124d86f9e71a8fa9481037aded9b8e911184
pawelmiklas <pablomiklas@gmail.com>
2019-01-28 18:51:55 +0100

    first commit

---
+++ src/Lol.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './lol.css';
+
+class Lol extends Component {
+    render() {
+        return (
+            <div className="xd">
+                hejka
+            </div>
+        );
+    }
+}
+
+export default Lol;
